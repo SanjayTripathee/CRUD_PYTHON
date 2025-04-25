65a5cbe8efe68f4deac0878d9237d9bc180d5aeb
sanjay <st1706851@gmail.com>
2025-04-25 08:07:34 +0545

    error_handeling pratice

---
+++ error_handeling/error_handeling.py
@@ -0,0 +1,13 @@
+# file = open("youtube.txt","w")
+
+# try:
+#     file.write("I am Sanjay")
+
+# finally:
+#     file.close()
+
+
+# or,
+
+# with open("youtube.txt","w") as file:
+#     file.write("I am coded ")
---
+++ error_handeling/youtube_manager.py
@@ -0,0 +1,9 @@
+
+
+while True:
+   print("\n Youtube Manager | Choose an option")
+   print("1. List all Youtube videos")
+   print("2. Add a new video")
+   print("3. Update a youtube video")
+   print("4. Delete a youtube video")
+   print("5. Exit the app")
