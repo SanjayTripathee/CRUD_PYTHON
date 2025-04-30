d292c92f9d0bddd525840f429fbc8a472372c568
sanjay <st1706851@gmail.com>
2025-04-30 07:26:01 +0545

    small project complete

--- error_handeling/youtube_manager.py
+++ error_handeling/youtube_manager.py
@@ -4,8 +4,8 @@ def load_data():
     try:
         with open("youtube.txt", "r") as file:
             test =  json.load(file)
-            print(type(test))
             return test
+ 
     except FileNotFoundError:# this is like catch block and FileNotFoundError is error type (like we reade in java in exceptionHandeling)
         return []
 
@@ -14,8 +14,12 @@ def save_data_helper(videos):
         json.dump(videos, file)
 
 def list_all_videos(videos):
-    for index, video in enumerate(videos,start=1):
-          print(f"{index}.")
+    print("\n")
+    print("*"*50)
+    for index,video in enumerate(videos,start=1):
+          print(f"{index}.{video["name"]} Duration: {video['time']}")
+    print("\n")
+    print("*"*50)
 
 def add_video(videos):
     name = input("Enter video name: ")
@@ -23,10 +27,25 @@ def add_video(videos):
     videos.append({"name": name, "time": time})
     save_data_helper(videos)
 def update_video(videos):   
-    pass
+    list_all_videos(videos)# 1st we get all list of video so , we call this function
+    index = int(input("Enter video index to update: "))
+    if 1 <=index <= len(videos):# here we check that index is greater than 0 and index is less than length of videos
+        name = (input("Enter video name: "))
+        time = (input("Enter video time: "))
+        videos[index-1] = {"name": name, "time": time}# to which index to update
+        save_data_helper(videos)
+    else:
+        print("Invalid index")
+       
+   
+    
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)# 1st we get all list of video so , we call this function
+    index = int(input("Enter video index to delete: "))
+    if 1 <=index <= len(videos):# here we check that index is greater than 0 and index is less than length of videos
+         del videos[index-1] # useing del keyword we delete the videos
+         save_data_helper(videos)
 
 def main():# # We define this main function so we can specify that the application starts from here.
     videos = load_data()
@@ -39,7 +58,7 @@ def main():# # We define this main function so we can specify that the applicati
         print("5. Exit the app")
 
         choice = input("Enter your choice: ")
-        print(videos)
+        # print(videos)
         match choice:
             case "1":
                 list_all_videos(videos)
